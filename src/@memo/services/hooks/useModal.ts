284aa21328f1d1d9aad185aa5ccd8e5a7042c174
ANNA KUZMENKO <anna.kuzmenko120063@gmail.com>
2024-09-08 01:34:37 +0300

    change image src

--- src/@memo/services/hooks/useModal.ts
+++ src/@memo/services/hooks/useModal.ts
@@ -2,9 +2,9 @@ import {useState} from 'react';
 
 export const useModal = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const toggleModal = () => {
     setIsOpen((prevState) => !prevState);
-    console.log(isOpen);
   };
 
   return {
